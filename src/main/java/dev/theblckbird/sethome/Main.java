3cbdf9c4208a311367a0306e180494effe211748
TheBlckbird <theblckbrd@proton.me>
2023-12-16 19:55:23 +0100

    initial commit

---
+++ src/main/java/dev/theblckbird/sethome/Main.java
@@ -0,0 +1,13 @@
+package com.theblckbird.sethome;
+
+public class Main {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
+}
